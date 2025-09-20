0fb023d162fd0914e2e6b0baf2bc36e5ce377def
macreai <ardaardiansyah24@gmail.com>
2025-09-20 08:48:57 +0700

    commit

--- python/consts/app.py
+++ python/consts/app.py
@@ -1,12 +1,9 @@
 from docling.document_converter import DocumentConverter
 from pathlib import Path
-from docling.chunking import HybridChunker
 from FlagEmbedding import BGEM3FlagModel
 
 converter = DocumentConverter()
 
 output_file = Path("output.md")
-
-chunker = HybridChunker()
 
 embedding_model = BGEM3FlagModel('BAAI/bge-m3',  use_fp16=True)
